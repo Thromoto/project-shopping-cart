00e86047f7da3151bdd3c1d70b4e37b3935b2834
Thiago Omoto <thi.omoto@gmail.com>
2022-09-12 18:54:08 -0300

    requisito 1 done

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,13 @@
-const fetchProducts = () => {
+const fetchProducts = async (comput) => {
   // seu código aqui
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${comput}`;
+  try {
+    const request = await fetch(url);
+    const response = await request.json();
+    return response;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
